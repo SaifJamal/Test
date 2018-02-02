1ded52d393fe4d714fa43de341ddedeb7e774258
Saif Jamal <saifj@name134p08.engin.umich.edu>
2018-02-02 16:31:40 -0500

    Another file

--- tester2.py
+++ tester2.py
@@ -3,3 +3,7 @@
 a = 5
 c = 3
 print(str(a+c))
+
+# Here are some additional changes made to this file
+d = 10
+print(str(a**2+b+c))
---
+++ tester3.py
@@ -0,0 +1,3 @@
+# This is the meaning of life
+
+print('Meaning of life!!!')
