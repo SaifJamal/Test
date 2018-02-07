6bc5143b711a1d9755496c90a153629873502836
Saif Jamal <saifj@srb2230p11.engin.umich.edu>
2018-02-06 21:33:16 -0500

    Dual lines

--- lol.py
+++ lol.py
@@ -6,9 +6,11 @@ cl = 1.1
 v = np.linspace(0, 30)
 rho = 1.225
 L = 0.5*rho*v**2*S*cl
+L2 = 0.5*rho*v**2*S*0.9
 
 plt.figure()
 plt.plot(v, L)
+plt.plot(v, L2)
 plt.xlabel('Velocity [m/s]')
 plt.ylabel('Lift [N]')
 plt.show()
