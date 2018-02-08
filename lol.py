f602da5223c0cf07eb5bd319f373d297d7d0f1c2
Saif Jamal <saifj@srb2230p22.engin.umich.edu>
2018-02-07 20:34:29 -0500

    Modified lines

--- lol.py
+++ lol.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import optimize
 
 S = 50
 cl = 1.1
@@ -7,10 +8,12 @@ v = np.linspace(0, 30)
 rho = 1.225
 L = 0.5*rho*v**2*S*cl
 L2 = 0.5*rho*v**2*S*0.9
+L3 = 0.5*rho*v**2
 
 plt.figure()
 plt.plot(v, L)
 plt.plot(v, L2)
+plt.plot(v, L3)
 plt.xlabel('Velocity [m/s]')
 plt.ylabel('Lift [N]')
 plt.show()
